d33ceb2b01a24cc251a3b3f2a395b54594f52221
Wahid Kamruddin <wahidkamruddin101@gmail.com>
2023-07-28 10:31:45 -0400

    Initial Commit

---
+++ rainy-or-sunny/rainy_or_sunnyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rainy_or_sunnyApp.swift
+//  rainy-or-sunny
+//
+//  Created by Wahid Kamruddin on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct rainy_or_sunnyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rainy-or-sunnyTests/rainy_or_sunnyTests.swift
@@ -0,0 +1,35 @@
+//
+//  rainy_or_sunnyTests.swift
+//  rainy-or-sunnyTests
+//
+//  Created by Wahid Kamruddin on 7/28/23.
+//
+
+import XCTest
+
+final class rainy_or_sunnyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
